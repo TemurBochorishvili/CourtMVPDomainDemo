cb5ae3e76d8b0e7ec2fa9d3bdb0eb2dcc0245003
Teimuraz Botchorishvili <bochorishvili.teimuraz@outlook.com>
2022-07-18 10:12:13 +0400

    started working on intercession refactoring.

--- Order.fs
+++ Order.fs
@@ -1,45 +1,34 @@
-type State<'IntercessionId, 'LibelId, 'CaseId> =
-    State of Order<'IntercessionId, 'LibelId, 'CaseId> voption
+type State = State of Order voption
 
-and Order<'IntercessionId, 'LibelId, 'CaseId> =
-    { Status: OrderStatus;
-      Acceptance: Acceptance voption;
-      Type: OrderType<'IntercessionId, 'LibelId, 'CaseId> }
-
-and OrderStatus =
+and Order =
 | New
-| Final of FinalOrderStatus
+| Final of FinalOrder
 
-and Acceptance = Acceptance
-
-and FinalOrderStatus =
-| Unpublished of UnpublishedFinalOrder
+and FinalOrder =
 | Published
+| Unpublished
 
-and UnpublishedFinalOrder =
-    { Time: unit }
-
-and OrderType<'IntercessionId, 'LibelId, 'CaseId> =
-| AttachedToIntercession of 'IntercessionId
-| AttachedToLibel of 'LibelId
-| AttachedToCase of 'CaseId
-
-// განჩინება რომელიც წინაპირობის გარეშე შეუძლია გამოიტანოს +
-// განჩინებას ეხლა აქ ესეთი სთეითები:
-    // შექმნილი
-    // საბოლოო (საბოლოო შეიძლება მისცეს რო 2 საათში გაუშვიო და ამ 2 საათში შეუძლია საბოლოო მოხსნას)
-        // გამოქვეყნებული
-        // გამოუქვეყნებელი
-    // კანონიერ ძალაში შესვლის თარიღი(გვჭირდება თუ არა ამ სერვისში)
-    // ? ცნობის საკითხი ?
-        // თამარასთან გავარკვიო ეს ცნობა რა ფორმატისაა და საჭიროა მერე მაგის ცალკე გატანა?
 
 type Command =
+| CreateIntercessionOrder
+| MarkIntercessionOrderAsFinal
 | PublishIntercessionOrder
-| PublishLibelOrder
+| UnmarkIntercessionOrderFinal
+
+| CreateCaseOrder
+| MarkCaseOrderAsFinal
 | PublishCaseOrder
+| UnmarkCaseOrderFinal
+
 
 type Event =
+| CreatedIntercessionOrder
+| MarkedIntercessionOrderAsFinal
 | PublishedIntercessionOrder
-| PublishedLibelOrder
+| UnmarkedIntercessionOrderFinal
+
+| CreateedCaseOrder
+| MarkedCaseOrderAsFinal
 | PublishedCaseOrder
+| UnmarkedCaseOrderFinal
+

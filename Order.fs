927a53c962158c9b6814277d5174d9dd1f95036c
Temur Bochorishvili <bochorishvili.teimuraz@outlook.com>
2022-07-18 14:32:35 +0400

    order refactored.

--- Order.fs
+++ Order.fs
@@ -1,34 +1,65 @@
-type State = State of Order voption
+type State<'IntercessionKey, 'CaseKey> =
+    State of Order<'IntercessionKey, 'CaseKey> voption
 
-and Order =
-| New
+and Order<'IntercessionKey, 'CaseKey> =
+    { Type: OrderType<'IntercessionKey, 'CaseKey>;
+      Status: OrderStatus }
+
+and OrderType<'IntercessionKey, 'CaseKey> =
+| AttachedToIntercession of AttachedToIntercessionOrderType<'IntercessionKey>
+| AttachedToCase of AttachedToCaseOrderKey<'CaseKey>
+
+and AttachedToIntercessionOrderType<'IntercessionKey> =
+    { Key: 'IntercessionKey }
+and AttachedToCaseOrderKey<'CaseKey> =
+    { Key: 'CaseKey }
+
+and OrderStatus =
+| Pending
 | Final of FinalOrder
 
 and FinalOrder =
+    { PublicationDate: unit;
+      Status: FinalOrderStatus }
+
+and FinalOrderStatus =
 | Published
 | Unpublished
 
 
-type Command =
-| CreateIntercessionOrder
+
+
+type Command<'IntercessionKey, 'CaseKey> =
+| CreateIntercessionOrder of CreateIntercessionOrderCommand<'IntercessionKey>
 | MarkIntercessionOrderAsFinal
 | PublishIntercessionOrder
 | UnmarkIntercessionOrderFinal
-
-| CreateCaseOrder
+| CreateCaseOrder of CreateCaseOrderCommand<'CaseKey>
 | MarkCaseOrderAsFinal
 | PublishCaseOrder
 | UnmarkCaseOrderFinal
 
+and CreateIntercessionOrderCommand<'IntercessionKey> =
+    { Key: 'IntercessionKey }
 
-type Event =
-| CreatedIntercessionOrder
+and CreateCaseOrderCommand<'CaseKey> =
+    { Key: 'CaseKey }
+
+
+
+
+type Event<'IntercessionKey, 'CaseKey> =
+| CreatedIntercessionOrder of CreatedIntercessionOrderEvent<'IntercessionKey>
 | MarkedIntercessionOrderAsFinal
 | PublishedIntercessionOrder
 | UnmarkedIntercessionOrderFinal
-
-| CreateedCaseOrder
+| CreateedCaseOrder of CreatedCaseOrderEvent<'CaseKey>
 | MarkedCaseOrderAsFinal
 | PublishedCaseOrder
 | UnmarkedCaseOrderFinal
 
+and CreatedIntercessionOrderEvent<'IntercessionKey> =
+    { Key: 'IntercessionKey }
+
+and CreatedCaseOrderEvent<'CaseKey> =
+    { Key: 'CaseKey }

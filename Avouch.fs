d7b2bcb727a467d57eff7618ebeeabe3c9f358d9
Temur Bochorishvili <bochorishvili.teimuraz@outlook.com>
2022-07-18 16:02:12 +0400

    refactored avouch

--- Avouch.fs
+++ Avouch.fs
@@ -1,21 +1,70 @@
-type State<'IntercessionId, 'LibelId, 'CaseId> =
-    State of Avouch<'IntercessionId, 'LibelId, 'CaseId> voption
+type State<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> =
+    State of Avouch<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> voption
 
-type Avouch<'IntercessionId, 'LibelId, 'CaseId> =
-    { Status: AvouchStatus<'IntercessionId, 'LibelId, 'CaseId> }
+and Avouch<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> =
+    { Status: AvouchStatus<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> }
 
-and AvouchStatus<'IntercessionId, 'LibelId, 'CaseId> =
+and AvouchStatus<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> =
 | New
-| DistributedToSecondaryOffice
-| Attached of AvouchAttachStatus<'IntercessionId, 'LibelId, 'CaseId>
+| DistributedToSecondaryOffice of DistributeAvouchToSecondaryOffice<'SecondaryOfficeEmployeeKey>
+| Attached of AvouchAttachStatus<'IntercessionKey, 'CaseKey>
 
-and AvouchAttachStatus<'IntercessionId, 'LibelId, 'CaseId> =
-    { React: ReactedAvouch voption;
-      Type: AvouchType<'IntercessionId, 'LibelId, 'CaseId> }
+and DistributeAvouchToSecondaryOffice<'SecondaryOfficeEmployeeKey> =
+    { Key: 'SecondaryOfficeEmployeeKey }
 
-and AvouchType<'IntercessionId, 'LibelId, 'CaseId> =
-| ToIntercession of 'IntercessionId
-| ToLibel of 'LibelId
-| ToCase of 'CaseId
+and AvouchAttachStatus<'IntercessionKey, 'CaseKey> =
+    { Type: AvouchType<'IntercessionKey, 'CaseKey>;
+      React: ReactedAvouch voption }
+
+and AvouchType<'IntercessionKey, 'CaseKey> =
+| Intercession of 'IntercessionKey
+| Case of 'CaseKey
 
 and ReactedAvouch = ReactedAvouch
+
+
+
+
+type Command<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> =
+| CreateAvouch
+| DistributeAvouchToSecondaryOffice of DistributeAvouchToSecondaryOfficeCommand<'SecondaryOfficeEmployeeKey>
+| AttachAvouchToIntercession of AttachAvouchToIntercessionCommand<'IntercessionKey>
+| AttachAvouchToCase of AttachAvouchToCaseCommand<'CaseKey>
+| ReactOnAvouch
+
+and DistributeAvouchToSecondaryOfficeCommand<'SecondaryOfficeEmployeeKey> =
+    { Key: 'SecondaryOfficeEmployeeKey }
+
+and AttachAvouchToIntercessionCommand<'IntercessionKey> =
+    { Key: 'IntercessionKey }
+
+and AttachAvouchToCaseCommand<'CaseKey> =
+    { Key: 'CaseKey }
+
+
+
+
+type Event<'SecondaryOfficeEmployeeKey, 'IntercessionKey, 'CaseKey> =
+| CreateedAvouch of CreatedEvent
+| DistributeedAvouchToSecondaryOffice of DistributedAvouchToSecondaryOfficeEvent<'SecondaryOfficeEmployeeKey>
+| AttachedAvouchToIntercession of AttachedAvouchToIntercessionEvent<'IntercessionKey>
+| AttachedAvouchToCase of AttachedAvouchToCaseEvent<'CaseKey>
+| ReactOnAvouch of ReactOnAvouchEvent
+
+and CreatedEvent =
+    { Time: unit }
+
+and DistributedAvouchToSecondaryOfficeEvent<'SecondaryOfficeEmployeeKey> =
+    { Key: 'SecondaryOfficeEmployeeKey;
+      Time: unit }
+
+and AttachedAvouchToIntercessionEvent<'IntercessionKey> =
+    { Key: 'IntercessionKey;
+      Time: unit }
+
+and AttachedAvouchToCaseEvent<'CaseKey> =
+    { Key: 'CaseKey;
+      Time: unit }
+
+and ReactOnAvouchEvent =
+    { Time: unit }
